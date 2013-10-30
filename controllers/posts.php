549d4cc3980b4dcf588077750aaf3a9275c95a9a
Elina <elinapuman@gmail.com>
2013-10-30 09:33:51 +0200

    Add posts listing functionality

--- controllers/posts.php
+++ controllers/posts.php
@@ -1,8 +1,8 @@
 <?php
 
-class welcome extends Controller{
+class posts extends Controller{
 
 	function index(){
-		$this->users = get_all("SELECT * FROM user");
+		$this->posts = get_all("SELECT * FROM post");
 	}
 }
--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,5 +1,10 @@
-<ul>
-	<li>1. Rename welcome controller enda põhikontrolleri nimeliseks (ära unusta classi nime muuta).</li>
-	<li>3. Kohanda views/templates/master_template.php failis pealkiri ja projekti nimi.</li>
-	<li>4. Loo enda põhikontrolleri jaoks vaatefail (näiteks views/products_index_view.php).</li>
-</ul>
+<div class="span8">
+	<?foreach($posts as $post):?>
+	<h1><?=$post['post_subject']?></h1>
+	<p><?=$post['post_text']?></p>
+	<div>
+		<span class="badge badge-success">Posted <?=$post['post_created']?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+	</div>
+	<hr>
+	<?endforeach?>
+</div>
